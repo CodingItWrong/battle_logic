e448bf9d1e0486f18ac180149d744a8a637bc5de
Josh Justice <jjustice@bignerdranch.com>
2016-07-22 11:51:59 -0400

    Simplify character spec

--- spec/units/character_spec.rb
+++ spec/units/character_spec.rb
@@ -60,48 +60,6 @@ module BattleLogic
       expect(subject).to be_alive
     end
 
-    it "loses current health when it receives damage" do
-      subject = described_class.new(max_health:3)
-      subject.receive_damage!
-      expect(subject.current_health).to eq(2)
-    end
-
-    it "can be hit for multiple damage" do
-      subject = described_class.new(max_health:3)
-      subject.receive_damage!(2)
-      expect(subject.current_health).to eq(1)
-    end
-
-    it "does not allow damage less than zero" do
-      subject = described_class.new(max_health:3)
-      subject.receive_damage!(-1)
-      expect(subject.current_health).to eq(3)
-    end
-
-    it "cannot have current health less than zero" do
-      subject = described_class.new(max_health:1)
-      subject.receive_damage!(2)
-      expect(subject.current_health).to eq(0)
-    end
-    
-    it "gains health when it receives healing" do
-      subject = described_class.new(max_health: 10, current_health: 5)
-      subject.receive_healing!(2)
-      expect(subject.current_health).to eq(7)
-    end
-    
-    it "cannot gain more health than its max health" do
-      subject = described_class.new(max_health: 10, current_health: 5)
-      subject.receive_healing!(20)
-      expect(subject.current_health).to eq(10)
-    end
-    
-    it "cannot be healed a negative amount" do
-      subject = described_class.new(max_health: 10, current_health: 5)
-      subject.receive_healing!(-1)
-      expect(subject.current_health).to eq(5)
-    end
-
     it "can attack" do
       subject = described_class.new(attack_rating:2)
       defender = double("defender", defense_rating: 1, receive_damage!: nil)
@@ -110,49 +68,78 @@ module BattleLogic
       expect(defender).to have_received(:receive_damage!).with(1)
     end
     
-    it "can be configured with a use item action" do
-      use_item = double("use item", perform:nil)
-      use_item_class = double("use item class", new: use_item)
-      inventory = double("inventory", contain?: true, remove: nil)
-      subject = described_class.new(use_item_action: use_item_class, inventory: inventory)
-      item = double("item")
-      target = double("target")
-      
-      subject.use(item, on: target)
-      
-      expect(use_item_class).to have_received(:new).with(item: item, target: target)
-      expect(use_item).to have_received(:perform)
-    end
+    context "receiving damage" do
+      it "loses current health when it receives damage" do
+        subject = described_class.new(max_health:3)
+        subject.receive_damage!
+        expect(subject.current_health).to eq(2)
+      end
 
-    it "has an inventory" do
-      inventory = double("inventory")
-      subject = described_class.new(inventory: inventory)
-      expect(subject.inventory).to eq(inventory)
-    end
+      it "can be hit for multiple damage" do
+        subject = described_class.new(max_health:3)
+        subject.receive_damage!(2)
+        expect(subject.current_health).to eq(1)
+      end
 
-    it "cannot use items not in the inventory" do
-      use_item = double("use item", perform:nil)
-      use_item_class = double("use item class", new: use_item)
-      inventory = double("inventory", contain?: false)
-      subject = described_class.new(use_item_action: use_item_class, inventory: inventory)
-      item = double("item")
-      target = double("target")
-      
-      expect{ subject.use(item, on: target) }.to raise_error "item not in user's inventory"
+      it "does not allow damage less than zero" do
+        subject = described_class.new(max_health:3)
+        subject.receive_damage!(-1)
+        expect(subject.current_health).to eq(3)
+      end
+
+      it "cannot have current health less than zero" do
+        subject = described_class.new(max_health:1)
+        subject.receive_damage!(2)
+        expect(subject.current_health).to eq(0)
+      end
     end
     
-    it "removes items from the inventory when used" do
-      use_item = double("use item", perform:nil)
-      use_item_class = double("use item class", new: use_item)
-      inventory = double("inventory")
-      item = double("item")
-      target = double("target")
+    context "receiving healing" do
+      it "gains health when it receives healing" do
+        subject = described_class.new(max_health: 10, current_health: 5)
+        subject.receive_healing!(2)
+        expect(subject.current_health).to eq(7)
+      end
       
-      allow(inventory).to receive(:contain?).and_return(true)
-      expect(inventory).to receive(:remove).with(item)
+      it "cannot gain more health than its max health" do
+        subject = described_class.new(max_health: 10, current_health: 5)
+        subject.receive_healing!(20)
+        expect(subject.current_health).to eq(10)
+      end
       
-      subject = described_class.new(use_item_action: use_item_class, inventory: inventory)
-      subject.use(item, on: target)      
+      it "cannot be healed a negative amount" do
+        subject = described_class.new(max_health: 10, current_health: 5)
+        subject.receive_healing!(-1)
+        expect(subject.current_health).to eq(5)
+      end
+    end
+
+    context "using items" do
+      
+      subject(:character) { described_class.new(use_item_action: use_item_class,
+                                                inventory: inventory) }
+      let(:use_item_class) { double("use item class", new: use_item) }
+      let(:use_item) { double("use item", perform:nil) }
+      let(:inventory) { double("inventory", contain?: true, remove: nil) }
+      let(:item) { double("item") }
+      let(:target) { double("target") }
+      let(:use!) { subject.use(item, on: target) }
+    
+      it "can be configured with a use item action" do
+        use!
+        expect(use_item_class).to have_received(:new).with(item: item, target: target)
+        expect(use_item).to have_received(:perform)
+      end
+
+      it "cannot use items not in the inventory" do
+        allow(inventory).to receive(:contain?).and_return(false)
+        expect{ use! }.to raise_error "item not in user's inventory"
+      end
+      
+      it "removes items from the inventory when used" do
+        use!
+        expect(inventory).to have_received(:remove).with(item)
+      end
     end
   end
 end
