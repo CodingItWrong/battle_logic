5122b0e908e72b159fed2706a80297df1f959cd2
Josh Justice <jjustice@bignerdranch.com>
2016-07-28 11:08:41 -0400

    Fix block chaining

--- lib/battle_logic.rb
+++ lib/battle_logic.rb
@@ -1,6 +1,7 @@
 lib_files = Dir[File.dirname(__FILE__) + '/battle_logic/*.rb']
-lib_files.map do |file|
+lib_paths = lib_files.map do |file|
   File.basename(file, File.extname(file))
-end.each do |file|
+end
+lib_paths.each do |file|
   require "battle_logic/#{file}"
 end
